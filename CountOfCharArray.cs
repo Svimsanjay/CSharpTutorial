00d9c38f195297998a48904562ed7c1d29a5a19c
Svimsanjay <jaypatidar31@gmail.com>
2024-01-02 18:06:13 +0530

    adding new class count of char array

---
+++ CountOfCharArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CSharpLearning
+{
+	public class CountOfCharArray
+	{
+        //public static void Main(String[] args)
+        //{
+        //    char[] arr = new char[] { 'a', 'b', 'c', 'a', 'b', 'd', 'f', 'c' };
+        //    var count = arr.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        //    foreach (var cha in count)
+        //        Console.WriteLine($"{cha.Key}-{cha.Value}");
+        //    Console.ReadLine();
+        //}
+    }
+}
+
